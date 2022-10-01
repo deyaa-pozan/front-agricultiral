a9e19de9be6c76994bdb3cce3b72504de9fb3c6f
deyaa-eldeen <dpozan@connectedmotion.io>
2022-10-01 16:41:14 +0300

    final

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+let token = cookies.get("token");
 
 export default axios.create({
-    baseURL: 'http://localhost:8081'
+    baseURL: 'https://agricultiral.herokuapp.com',
+    headers: {'Authorization': 'Bearer '+token}
 });
 
 // https://agricultiral.herokuapp.com
